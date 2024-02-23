d341217523f53e89a444f5c6b0a1bf04870be9e8
priyajevale <priyajevale0625@gmail.com>
2024-02-24 00:51:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 // import './App.css';
+
 import { Fragment } from 'react';
 import Header from './components/Layout/Header';
+import Meals from './components/Layout/Meals/Meals';
 function App() {
   return (
     
      <Fragment>  
       <Header/>
+      <main>
+        <Meals/>
+      </main>
      
       </Fragment>
     
