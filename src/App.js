3946c6e414ae5f0a108f6a2a6715e2764bafd89c
priyajevale <priyajevale0625@gmail.com>
2024-02-25 16:46:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@
 import { Fragment } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Layout/Meals/Meals';
+import Cart from './components/Layout/Meals/UI/Cart/Cart';
 function App() {
   return (
     
      <Fragment>  
+      <Cart/>
       <Header/>
       <main>
         <Meals/>
